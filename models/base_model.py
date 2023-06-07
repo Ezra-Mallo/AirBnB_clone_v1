22eaf2b9941cd46c3619e92aa01cde4b610a80ad
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-07 12:46:29 +0100

    Updated FileStorage and Base_Model and Console

--- models/base_model.py
+++ models/base_model.py
@@ -3,19 +3,25 @@
 
 from uuid import uuid4
 import datetime
+import models
 
 
 class BaseModel:
     """This is basemodel class"""
+    # id = str(uuid4())
+    # # created_at = datetime.datetime.now()
+    # # updated_at = datetime.datetime.now()
 
     def __init__(self, *args, **kwargs):
+        """Initialization of the instance
+            Args:
+                *args = argument variables
+                **kwargs = keyword variables
+            Raises:
+                none
         """
-        This is the initialization for this class
-        Argument:
-            args = liat
-            **kwargs = dictionary
-        """
-        if kwargs:
+        my_dict = {}
+        if kwargs is not None and len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
@@ -23,11 +29,11 @@ class BaseModel:
                         self.__dict__[key] = date_value
                     else:
                         self.__dict__[key] = value
-
         else:
             self.id = str(uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -42,8 +48,8 @@ class BaseModel:
         This updated the "updated_date" and saves it
         """
         self.updated_at = datetime.datetime.now()
-        # models.storage.new(self)
-        # models.storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """
